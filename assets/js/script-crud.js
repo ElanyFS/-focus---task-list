bb98fbe4e91ffcc036e4ccc58100f56bac08e585
Elany Ferreira <ellanysouza08@gmail.com>
2024-06-06 17:28:28 -0300

    funcionalidades js para os botões deletar e cancelar

--- assets/js/script-crud.js
+++ assets/js/script-crud.js
@@ -4,12 +4,22 @@ const formAddTask = document.querySelector(".form_task_add"); // formulario de a
 const textareaTask = document.querySelector(".textarea_add_task"); // tarefa informada para addconst ulElemento = document.querySelector('.dropdown_list'); // elemento ul
 const ulElemento = document.querySelector(".dropdown_list"); //elemento ul
 
+const btnCancelar = document.querySelector("#btn_cancelar"); // botao cancelar
+const btnDeletar = document.querySelector("#btn_deletar"); // botao deletar
+
 const taskList = JSON.parse(localStorage.getItem("tarefas")) || []; // Array de tarefas
+
+// Atualizar tarefas
+function atualizarTask() {
+  localStorage.setItem("tarefas", JSON.stringify(taskList)); // salvar no armazenamento local
+}
 
 // Criar elemento Li para exibir as tarefas na pagina
 function criarElementoLi(task) {
   const li = document.createElement("li");
   li.classList.add("task_list_li");
+
+  
 
   const paragrafo = document.createElement("p");
   paragrafo.classList.add("task_list_li_paragrafo");
@@ -18,6 +28,17 @@ function criarElementoLi(task) {
 
   const botao = document.createElement("button");
   botao.classList.add("task_list_li_btn");
+
+  botao.onclick = () => {
+    debugger;
+    const novaDescricao = prompt("Qual o novo nome da tarefa?");
+    console.log("tarefa", novaDescricao);
+    if (novaDescricao) {
+      paragrafo.textContent = novaDescricao;
+      task.descricao = novaDescricao;
+      atualizarTask();
+    }
+  };
 
   const icon = document.createElement("i");
 
@@ -44,7 +65,7 @@ formAddTask.addEventListener("submit", (event) => {
 
     taskList.push(task);
     ulElemento.append(criarElementoLi(task));
-    localStorage.setItem("tarefas", JSON.stringify(taskList)); // salvar no armazenamento local
+    atualizarTask();
     textareaTask.value = ""; //limpar formulario
     formAddTask.classList.add("invisible"); // esconde formulario
   }
@@ -54,3 +75,12 @@ formAddTask.addEventListener("submit", (event) => {
 taskList.forEach((task) => {
   ulElemento.append(criarElementoLi(task));
 });
+
+btnCancelar.onclick = () => {
+  textareaTask.value = "";
+};
+
+btnDeletar.onclick = () => {
+  textareaTask.value = "";
+  formAddTask.classList.add("invisible");
+};
